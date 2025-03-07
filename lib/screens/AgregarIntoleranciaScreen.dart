d6a0e2358801295dd37989a5589cd77bc1f8c682
Chavier <javiercastrorodriguez2005@gmail.com>
2025-03-07 09:15:36 +0100

    Añadido validador

--- lib/screens/AgregarIntoleranciaScreen.dart
+++ lib/screens/AgregarIntoleranciaScreen.dart
@@ -4,6 +4,7 @@ import 'package:nutriflow_app/services/firebase_service_intolerancias.dart';
 class AgregarIntoleranciaScreen extends StatelessWidget {
   final TextEditingController _controller = TextEditingController();
   final IntoleranciasService _service = IntoleranciasService();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -11,23 +12,32 @@ class AgregarIntoleranciaScreen extends StatelessWidget {
       appBar: AppBar(title: Text('Agregar Intolerancia')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _controller,
-              decoration: InputDecoration(labelText: 'Nombre de la Intolerancia'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                if (_controller.text.isNotEmpty) {
-                  await _service.agregarIntolerancia(_controller.text);
-                  Navigator.pop(context);
-                }
-              },
-              child: Text('Guardar'),
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _controller,
+                decoration: InputDecoration(labelText: 'Nombre de la Intolerancia'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'El campo no puede estar vacío';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    await _service.agregarIntolerancia(_controller.text);
+                    Navigator.pop(context);
+                  }
+                },
+                child: Text('Guardar'),
+              ),
+            ],
+          ),
         ),
       ),
     );
